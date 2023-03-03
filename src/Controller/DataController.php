62b5a65627e24f52ae16c207c40aa31f25c13fe0
Roobje <gj.vermeulen@hotmail.nl>
2023-03-03 16:03:26 +0100

    Datacontroller Read folder instead of files

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -5,22 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
 
 class DataController extends AbstractController
 {
     #[Route('/data', name: 'Kennisopdracht_data')]
     public function dataPage(): Response
     {
+        $finder = new Finder();
+        $finder->files()->in('C:\Users\gjver\Documents\Kennis opdracht\Kennisopdracht\data')->name('*.json');
 
-        // Load the contents of the JSON file into a variable
-        $jsonFile = file_get_contents('C:\Users\gjver\Documents\Kennis opdracht\Kennisopdracht\data\project-1.json');
+        $packages = [];
 
-        // Decode the JSON into an associative array
-        $data = json_decode($jsonFile, true);
+        foreach ($finder as $file) {
+            // Load the contents of the JSON file into a variable
+            $jsonFile = $file->getContents();
 
-        // Access the installed property on the array
-        $packages = $data['installed'];
+            // Decode the JSON into an associative array
+            $data = json_decode($jsonFile, true);
 
+            // Access the installed property on the array
+            $packages = array_merge($packages, $data['installed']);
+        }
 
         return $this->render('DataPage.html.twig', [
             'data' => $packages
