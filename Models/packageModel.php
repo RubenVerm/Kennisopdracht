15f05d7a11c2bcffae024108775f7dc150131421
Roobje <gj.vermeulen@hotmail.nl>
2023-03-03 15:12:03 +0100

    model for packages

---
+++ Models/packageModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class packageModel
+{
+    public $name;
+    public $directDependency;
+    public $homepage;
+    public $source;
+    public $version;
+    public $latest;
+    public $latestStatus;
+    public $description;
+    public $abandoned;
+}
